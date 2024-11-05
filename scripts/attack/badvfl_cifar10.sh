304ca1906a5eb708de0bdcdea3ceb83d3e81ac3b
Tim He <122210434+cowboyblur@users.noreply.github.com>
2024-11-05 17:47:08 +0800

    Update badvfl_cifar10.sh

--- scripts/attack/badvfl_cifar10.sh
+++ scripts/attack/badvfl_cifar10.sh
@@ -10,29 +10,35 @@ BASE_SAVE_PATH="${PROJECT_ROOT}/../results/models/BadVFL/cifar10"
 SCRIPT_PATH="${PROJECT_ROOT}/../attack/badvfl/badvfl_cifar10_training.py"
 
 # half参数列表
-HALF_VALUES=(14 16 18 20 22 24 26)
+# HALF_VALUES=(14 16 18 20 22 24 26)
 
-for half in "${HALF_VALUES[@]}"; do
-    save_path="${BASE_SAVE_PATH}/half_${half}"
-    
-    python ${SCRIPT_PATH} \
-        --dataset ${DATASET} \
-        --data_dir ${DATA_PATH} \
-        --half $half \
-        --report_freq 5 \
-        --workers 8 \
-        --epochs 80 \
-        --poison_budget 0.1 \
-        --corruption_amp 5.0 \
-        --optimal_sel \
-        --saliency_map_injection \
-        --pre_train_epochs 20 \
-        --trigger_train_epochs 40 \
-        --window_size 5 \
-        --backdoor_start \
-        --lr 0.02 \
-        --alpha 0.05 \
-        --eps 0.5 \
-        --batch_size 64 \
-        --save $save_path
+# for half in "${HALF_VALUES[@]}"; do
+WINDOW_SIZE_VALUE=(3 5)
+POISON_BUDGET_VALUE=(0.05 0.1 0.2)
+
+for window_size in "${WINDOW_SIZE_VALUE[@]}"; do
+    for poison_budget in "${POISON_BUDGET_VALUE[@]}"; do
+        save_path="${BASE_SAVE_PATH}/half_${half}"
+
+        python ${SCRIPT_PATH} \
+            --dataset ${DATASET} \
+            --data_dir ${DATA_PATH} \
+            --half 16 \
+            --report_freq 5 \
+            --workers 8 \
+            --epochs 80 \
+            --poison_budget $poison_budget \
+            --corruption_amp 5.0 \
+            --optimal_sel \
+            --saliency_map_injection \
+            --pre_train_epochs 20 \
+            --trigger_train_epochs 40 \
+            --window_size $window_size \
+            --backdoor_start \
+            --lr 0.02 \
+            --alpha 0.05 \
+            --eps 0.5 \
+            --batch_size 64 \
+            --save $save_path
+    done
 done
